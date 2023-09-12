9026b517a9828f406c6938fa0dcd7f6e3b295c43
Paul Parisot <paul.parisot@epitech.eu>
2023-09-12 23:42:44 +0200

    Fix: coding style on `src/debug.c`

--- src/debug.c
+++ src/debug.c
@@ -5,6 +5,7 @@
 void split_double(s4_t (*a)[], s8_t doublie)
 {
     unsigned int n = (sizeof(doublie) * 2);
+
     (*a)[0] = (s4_t)(doublie >> n * 2);
     (*a)[1] = ((s4_t)(doublie << n) >> n);
 }
@@ -12,9 +13,10 @@ void split_double(s4_t (*a)[], s8_t doublie)
 void register_debug(vm_t *vm, unsigned short n)
 {
     s1_t byt = vm->registersinfo[n];
-    bool is_neg = MASK(byt, 0b01000000);
+    bool is_neg = false;
     s4_t a[2] = {0};
 
+    is_neg = MASK(byt, 0b01000000);
     if (MASK(byt, 0b00000100) and not MASK(byt, 128))
         return __idc_return printf("%s\n", MASK(byt, 1) ? "true" : "false");
     if (byt == 0b00000010)
